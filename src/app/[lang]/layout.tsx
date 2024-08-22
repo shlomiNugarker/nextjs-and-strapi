afc9185305bccd7c67ccefa47395009b0ba04ce8
Shlomi Nugarker <shlomin1231@gmail.com>
2024-08-22 23:43:04 +0300

    add styles

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { i18n } from '../../../i18n-config'
+import '@/styles/main.scss'
 
 const inter = Inter({ subsets: ['latin'] })
 
