9dc01846a5fda15d3e75521665072d932ca3c5b0
Shlomi Nugarker <shlomin1231@gmail.com>
2024-08-28 12:50:04 +0300

    u

--- frontend/src/layouts/cmps/Hero.tsx
+++ frontend/src/layouts/cmps/Hero.tsx
@@ -70,7 +70,9 @@ export default function Hero({ data }: HeroProps) {
         </div>
         <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
           <Image
-            src={imgUrl || ''}
+            src={
+              'https://images.unsplash.com/photo-1723843038794-97663cc894ba?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+            }
             alt={
               data.picture.data.attributes.alternativeText || 'none provided'
             }
